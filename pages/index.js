f505e1357af1bbba6f83b16291a0de4de1429ff6
Jhon <59811681+okayda@users.noreply.github.com>
2023-05-09 08:56:40 -0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content={`Introduction of the photo gallery`}
+        />
+        <title>Photoshop Gallery</title>
+      </Head>
+      <h1>Welcome</h1>
+    </>
+  );
+}
