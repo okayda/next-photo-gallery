4b7287f1e4281513cc3b95c9136cb612a9934d9c
Jhon <59811681+okayda@users.noreply.github.com>
2023-05-14 22:54:21 -0700

    Add l-mobile media query

--- components/BoxsGrid/BoxsGrid.js
+++ components/BoxsGrid/BoxsGrid.js
@@ -5,23 +5,25 @@ import whiteArrow from "../../assets/arrow-white.svg";
 const BoxsGrid = function () {
   return (
     <section className={classes.boxs}>
-      {HomeBoxData.map((data) => (
-        <article>
-          <img src={data.img} alt={data.title} />
-          <div className={classes.boxs__wrapper}>
-            <div className={classes.boxs__contents}>
-              <a href="#">
-                <h3>{data.title}</h3>
-                <span>{data.author}</span>
-                <div>
-                  <span>read story</span>
-                  <img src={whiteArrow} alt="" />
-                </div>
-              </a>
+      <div className={classes.boxs__container}>
+        {HomeBoxData.map((data) => (
+          <article>
+            <img src={data.img} alt={data.title} />
+            <div className={classes.boxs__wrapper}>
+              <div className={classes.boxs__contents}>
+                <a href="#">
+                  <h3>{data.title}</h3>
+                  <span>{data.author}</span>
+                  <div>
+                    <span>read story</span>
+                    <img src={whiteArrow} alt="" />
+                  </div>
+                </a>
+              </div>
             </div>
-          </div>
-        </article>
-      ))}
+          </article>
+        ))}
+      </div>
     </section>
   );
 };
